f97be516663ffbc41cfb3ede95cd04820e95507b
Ravi Chandran <ravi.chandran@tringapps.com>
2024-08-12 18:17:39 +0530

    0.0.2

--- Sources/TringFrameWork/TringFrameWork.swift
+++ Sources/TringFrameWork/TringFrameWork.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public struct DoSomeFunction{
+    public static func DoSomeWork(){
+        print("Some Work is done")
+    }
+}
